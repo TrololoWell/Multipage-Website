2a51c85718df8828400a69ed0411b243f8ca7f01
TrololoWell <89628034+TrololoWell@users.noreply.github.com>
2022-06-23 11:54:34 +0300

    3h

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ window.addEventListener('scroll',() => (
    document.querySelector('nav').classList.toggle
    ('window-scroll', window.scrollY > 0)
 ))
+
+//show/hide faq
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+   faq.addEventListener('click', () => {
+      faq.classList.toggle('open');
+   })
+})
